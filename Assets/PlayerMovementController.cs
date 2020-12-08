5116415b53d2053faf16e8830384dafb79d4c86d
Jack <68773585+Ethirix@users.noreply.github.com>
2020-12-08 18:54:49 +0000

    v0.1.3
    Fixed exploit where you can jump far higher than intended.
    
    As a result increased JumpForce to reflect fix.

--- Assets/PlayerMovementController.cs
+++ Assets/PlayerMovementController.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb = default;
@@ -9,6 +12,17 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] private bool allowJumpMovement = default;
 
     private bool _isOnGround;
+    private delegate void JumpEvent();
+    private JumpEvent _jumpEvent;
+    private bool jumpTimeout = false;
+    
+    private void OnEnable() {
+        _jumpEvent += JumpEventFired;
+    }
+    private void OnDisable() {
+        _jumpEvent -= JumpEventFired;
+    }
+
     private void FixedUpdate()
     {
         if (_isOnGround) { rb.AddForce(0f, 0f, forwardForce); } else { rb.AddForce(0f, 0f, forwardForce * 0.5f); }
@@ -21,11 +35,22 @@ public class PlayerMovementController : MonoBehaviour
         
         //if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && _isOnGround) { rb.AddForce(0f, 0f, forwardForce); }
         //if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && _isOnGround) { rb.AddForce(0f, 0f, -forwardForce); }
-        
-        if (Input.GetKey(KeyCode.Space) && _isOnGround) { rb.AddForce(0f, 9.81f * rb.mass * jumpForce, 0f, ForceMode.VelocityChange); }
-        
+
+        if (Input.GetKey(KeyCode.Space) && _isOnGround && !jumpTimeout) {
+            rb.AddForce(0f, 9.81f * rb.mass * jumpForce, 0f, ForceMode.VelocityChange);
+            _jumpEvent.Invoke();
+        }
     }
 
+    private void JumpEventFired() {
+        jumpTimeout = true;
+        Invoke(nameof(ReenableJump), 0.1f);
+    }
+
+    private void ReenableJump() {
+        jumpTimeout = false;
+    }
+    
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject == floor) { _isOnGround = true; }
